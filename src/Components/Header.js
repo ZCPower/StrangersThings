9061e25f94a7ba45092b3468c2726c6673a4aac2
ZCPower <zachpowerxc@gmail.com>
2022-05-08 09:22:35 -0500

    First commit, practicing initiating a repo in the terminal

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default function Header() {
+
+    return (<header>
+        <h1>Stranger's Things</h1>
+        <nav>
+            <ul>
+                <li>Login</li>
+                <li>Register</li>
+                <li>Account</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa';
+
+export default function Main() {
+    return (
+        <main>
+            <input></input>
+            <button> <FaSearch /></button>
+        </main>
+    )
+}
